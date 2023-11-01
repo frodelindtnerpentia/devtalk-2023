0d242f7a141c6d716e8509ff83c40c2bc1051271
Frode Lindtner <frode@appstract.dk>
2023-11-01 17:03:36 +0100

    Illustrate converters and helpers

---
+++ FeatureTemperature/Converters/ConvertWeatherForecastResutls.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureTemperature.Converters
+{
+    public static class ConvertWeatherForecast
+    {
+        public static Models.TemperatureItem GetResults(Models.Transport.WeatherForecastItem input)
+        {
+            Models.TemperatureItem temperatureItem = new()
+            {
+                Date = input.date,
+                Temperature = Helpers.GetTemperature.GetCorrectTemperature(input),
+                Place = input.city
+            };
+
+            return temperatureItem;
+        }
+    }
+}
---
+++ FeatureTemperature/Helpers/GetTemperature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureTemperature.Helpers
+{
+    public static class GetTemperature
+    {
+        public static int GetCorrectTemperature(Models.Transport.WeatherForecastItem item)
+        {
+            return item.temperatureF;
+        }
+    }
+}
